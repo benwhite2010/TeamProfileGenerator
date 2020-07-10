a536ee4b37d2ea97ba151f6211fe1083eb53f3e3
Ben White <benwhite2010@gmail.com>
2020-07-09 21:19:30 -0500

    furthur update

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,8 +1,26 @@
 // TODO: Write code to define and export the Employee class
+class Employee {
+  constructor(name, id, email) {
+    name = this.name;
+    email = this.email;
+    id = this.id;
+  }
 
+  getName() {
+    return this.name;
+  }
 
+  getID() {
+    return this.id;
+  }
 
-
+  getEmail() {
+    return this.email;
+  }
+  getRole() {
+    return "Employee";
+  }
+}
 
 // export the Employee object
-module.exports = Employee
+module.exports = Employee;
